bb85eed01758ccb9bfe40c53b8141418386339f7
quyPham <turtlebeautiful96@gmail.com>
2025-02-09 23:07:01 +0700

    d

--- Server/src/controllers/paymentController.js
+++ Server/src/controllers/paymentController.js
@@ -4,10 +4,10 @@ const paymentControllerMoMo = async (req, res) => {
     try {
         const { amounts, method,userid } = req.body;
         if (!amounts || !method) {
-            return res.status(400).json({ status: "error", message: "Thiếu thông tin thanh toán",userid });
+            return res.status(400).json({ status: "error", message: "Thiếu thông tin thanh toán" });
         }
         
-        const result = await createPaymentMoMo(amounts, method);
+        const result = await createPaymentMoMo(amounts, method,userid);
         res.json({ status: "success", message: "Thanh toán thành công", link: result.link });
 
     } catch (error) {
